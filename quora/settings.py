4a0f144d92281f7dc318eb4c6a2e0ad0fb903f60
iammidhunpc <midhuntechversant123@>
2018-05-09 18:09:23 +0530

    mysql

--- quora/settings.py
+++ quora/settings.py
@@ -74,12 +74,27 @@ WSGI_APPLICATION = 'quora.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
+
 
 AUTH_USER_MODEL = 'login.Register'
 
