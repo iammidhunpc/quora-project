d91b7e19d38babe32b8a8e4b1ec21a60a4ad6029
iammidhunpc <midhuntechversant123@>
2018-05-15 10:07:23 +0530

    admin page modifications

--- login/views.py
+++ login/views.py
@@ -22,30 +22,7 @@ from django.contrib.auth.hashers import make_password
 
 
 
-# from django.contrib.auth.decorators import user_passes_test
-
-
-
-# @user_passes_test(lambda u: u.is_superuser)
-# def score_reset(self,...):
-#     ...
-
-
-
-
-
-
-class HandleMix(AccessMixin):
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated():
-            return self.handle_no_permission()
-        if not request.user.is_active:
-            return self.handle_not_activated()
-        return super(ActiveOnlyMixin, self).dispatch(request, *args, **kwargs)
-
-
-
-
+#USER REGISTRATION
 class IndexView(FormView):
     form_class = NameForm
     template_name = 'reg.html'
@@ -65,10 +42,7 @@ class IndexView(FormView):
         send_mail("123", "123", 'anjitha.test@gmail.com', [obj.email], html_message=msg_html, fail_silently=False)
         return super().form_valid(form)
 
-
-
-
-
+#REGISTERED USERS LIST
 class DisplayView(generic.TemplateView):
     template_name = 'view.html'
 
@@ -85,7 +59,7 @@ class DisplayView(generic.TemplateView):
         context['st5'] = st5
         return context
 
-
+#USER LOGIN
 class LogView(FormView):
     form_class = LogForm
     template_name = 'log.html'
@@ -119,6 +93,17 @@ class LogView(FormView):
         # Bad login details were provided. So we can't log the user in.
             return HttpResponseRedirect('/login/log')
 
+#MIXINS HANDLING
+class HandleMix(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return self.handle_no_permission()
+        if not request.user.is_active:
+            return self.handle_not_activated()
+        return super(ActiveOnlyMixin, self).dispatch(request, *args, **kwargs)
+
+
+#ADMIN PAGE
 
 class LogAdminView(FormView):
     form_class = LogadminForm
@@ -154,7 +139,7 @@ class LogAdminView(FormView):
             return HttpResponseRedirect('/login/adminlogin')
 
 
-
+#HOME PAGE
 class SuccessView(ActiveOnlyMixin,FormView):
     template_name = 'success.html'
     form_class = QuestForm
@@ -199,6 +184,7 @@ class SuccessView(ActiveOnlyMixin,FormView):
             messages.add_message(self.request, messages.WARNING, 'Your question is pending moderation from the admin !')
         return super(SuccessView,self).form_valid(form)  
 
+
 class FailedView(generic.TemplateView):
     template_name = 'failed.html'
 
@@ -206,17 +192,14 @@ class FailedView(generic.TemplateView):
 class AdminView(generic.TemplateView):
     template_name = 'admin_home.html'
 
+
 class HomeView(generic.TemplateView):
    
     obj1=Logs.objects.all()
 
     template_name = 'home.html'
 
-
-
-
-
-
+#ADMIN QUESTION APPROVAL
 class QuestapView(ActiveOnlyMixinadmin,FormView):
     template_name = 'questap.html'
     form_class = ActForm
@@ -231,21 +214,11 @@ class QuestapView(ActiveOnlyMixinadmin,FormView):
         st6=[]
         st7=[]
         st8=[]
-        obj=Quest.objects.all()
-        
-        
-
-        for j in obj:
-            st5.append(j.question)
-            st6.append(j.status)
-            st7.append(j.logid)
-            st8.append(j.id)
-            context['st5'] = st5
-            context['st6'] = st6
-            context['st7'] = st7
-            context['st8'] = st8  
+        obj=Quest.objects.all() 
         context['question']=obj
         return context
+
+#ADMIN QUESTIONS STATUS CHECKING 
 class StatusView(FormView):
      def dispatch(self, request, *args, **kwargs):
 
@@ -259,14 +232,9 @@ class StatusView(FormView):
             messages.add_message(self.request, messages.WARNING, 'Q. %s is approved !'%(obj3.question))
         return HttpResponseRedirect('/login/questap')
 
-    # def form_valid(self, form):
-    # 	quest1 = form.cleaned_data['stats']
-    # 	obj3=Quest.objects.filter(id=quest1)
-    # 	for i in obj3:
-    # 		i.status='1'
-    # 		i.save()
-    # 	return super().form_valid(form)
 
+
+#ADMIN ANSWER APPROVAL
 class AnsapView(ActiveOnlyMixinadmin,FormView):
     template_name = 'ansap.html'
     form_class = AnsstatForm
@@ -277,41 +245,29 @@ class AnsapView(ActiveOnlyMixinadmin,FormView):
     not_activated_redirect = 'home'
     def get_context_data(self, **kwargs):
         context = super(AnsapView, self).get_context_data(**kwargs)
-        st10=[]
-        st11=[]
-        st12=[]
-        st13=[]
-        st14=[]
-        st15=[]
         obj1=Ans.objects.all()
         obj=Quest.objects.all()
-        for i in obj1:
-            st10.append(i.id)
-            st11.append(i.answer)
-            st12.append(i.status)
-            st13.append(i.ques)
-            context['st10'] = st10
-            context['st11'] = st11
-            context['st12'] = st12
-            context['st13'] = st13
- 
+        context['answer']=obj1
+        context['question']=obj
         return context
 
 
+#ADMIN ANSWERS STATUS CHECKING 
 class StatusAnsView(FormView):
      def dispatch(self, request, *args, **kwargs):
 
         t = self.kwargs['statusansid']
         obj3=Ans.objects.get(id=t)
         if obj3.status == 'Approved':
-            messages.add_message(self.request, messages.WARNING, 'Ans. %s  is already approved!'%(obj3.answer))
+            messages.add_message(self.request, messages.WARNING, 'Answer %s  is already approved!'%(obj3.answer))
         else:
             obj3.status='Approved'
             obj3.save()
-            messages.add_message(self.request, messages.WARNING, 'Ans. %s is approved!'%(obj3.answer))
+            messages.add_message(self.request, messages.WARNING, 'Answer %s is approved!'%(obj3.answer))
         return HttpResponseRedirect('/login/ansap')     
         
 
+#USER ANSWERS 
 class AnswerView(ActiveOnlyMixin,FormView):
     template_name = 'answer.html'
     form_class = AnsForm
@@ -350,6 +306,7 @@ class AnswerView(ActiveOnlyMixin,FormView):
         return super(AnswerView,self).form_valid(form)
 
 
+#USERS COMMENTS
 class CommentView(ActiveOnlyMixin,FormView):
     template_name = 'comment.html'
     form_class = ComForm
@@ -378,16 +335,9 @@ class CommentView(ActiveOnlyMixin,FormView):
             obj5=Comm.objects.create(comment=comment1,ansid=tmp1,users=self.request.user.username)
             obj5.save()
             return super(CommentView,self).form_valid(form)
-# class DeleteView(generic.TemplateView):
-#     template_name = 'success.html'
-#     success_url='/login/success'
-#     def get_context_data(self, *args, **kwargs):
-#         context = super(DeleteView,self).get_context_data(**kwargs)
-#         did = self.kwargs['did']
-#         q_obj = Quest.objects.filter(id=did)
-#         q_obj.delete()
-#     def get_success_url(self, **kwargs):         
-#         return reverse_lazy('/login/success', kwargs = {'pk': self.kwargs['did']})
+
+
+#DELETE QUESTIONS
 class DeleteView(ActiveOnlyMixin,generic.TemplateView):
     template_name = 'success.html'
     success_url='/login/success'
@@ -407,6 +357,9 @@ class DeleteView(ActiveOnlyMixin,generic.TemplateView):
             else:
                 messages.add_message(self.request, messages.WARNING, 'You cant delete other users question !')
                 return HttpResponseRedirect('/login/success')
+
+
+#DELETE ANSWERS
 class DeleteansView(ActiveOnlyMixin,generic.TemplateView):
     template_name = 'success.html'
     success_url='/login/success'
@@ -427,7 +380,7 @@ class DeleteansView(ActiveOnlyMixin,generic.TemplateView):
                 return HttpResponseRedirect('/login/success')
 
 
-
+#EMAIL REGISTRATION CONFIRMATION
 class RegistrationSuccess(generic.TemplateView):
     template_name = 'registration-success.html'
 
